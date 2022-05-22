e83a1e32e712231309e25dee1d263e1db65bdea8
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-05-22 23:32:27 +0300

    App was completed.

--- app/src/main/java/com/metehanbolat/calculatorcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/calculatorcompose/MainActivity.kt
@@ -3,23 +3,34 @@ package com.metehanbolat.calculatorcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.metehanbolat.calculatorcompose.ui.theme.CalculatorComposeTheme
+import com.metehanbolat.calculatorcompose.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorComposeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                }
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                )
             }
         }
     }
